4ebc5215d3a98d5383efeaadaa2677ac1100c4b0
Prasanna <prasanna.sp2540@gmail.com>
2026-09-11 23:40:23 +0530

    Add files via upload

---
+++ Experiment10.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+public class Experiment5 {
+
+    static class Player implements Comparable<Player> {
+
+        private String name;
+        private int matchesPlayed;
+        private double battingAverage;
+        private boolean injured;
+
+        public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
+            this.name = name;
+            this.matchesPlayed = matchesPlayed;
+            this.battingAverage = battingAverage;
+            this.injured = injured;
+        }
+
+        static boolean isDraftable(int matchesPlayed) {
+            return matchesPlayed >= 10;
+        }
+
+        static boolean isDraftable(int matchesPlayed, boolean injured) {
+            return matchesPlayed >= 5 && !injured;
+        }
+
+        public int compareTo(Player other) {
+            return Double.compare(other.battingAverage, this.battingAverage);
+        }
+    }
+
+    static String draftAndRank(Player[] players) {
+
+        Player[] draftable = new Player[players.length];
+        int count = 0;
+
+        for (Player p : players) {
+            if (Player.isDraftable(p.matchesPlayed) ||
+                Player.isDraftable(p.matchesPlayed, p.injured)) {
+                draftable[count++] = p;
+            }
+        }
+
+        Arrays.sort(draftable, 0, count);
+
+        String result = "";
+
+        for (int i = 0; i < count; i++) {
+            result += (i + 1) + ". " + draftable[i].name;
+
+            if (i < count - 1)
+                result += " | ";
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Player[] players = {
+            new Player("Virat", 15, 48.0, false),
+            new Player("Rahul", 7, 55.0, false),
+            new Player("Sameer", 3, 60.0, false),
+            new Player("Dev", 12, 20.0, true)
+        };
+
+        System.out.println(draftAndRank(players));
+    }
+}
